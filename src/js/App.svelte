35038a75f3ef8d72011f440fc5c5fd6f25c354b3
Patrick Judge <pjudge@bettergov.org>
2019-06-05 19:22:57 -0500

    delete commented-out code

--- src/js/App.svelte
+++ src/js/App.svelte
@@ -7,12 +7,3 @@
 <section class="story">
   {@html Dummy.html()}
 </section>
-
-<!-- <label>
-  <input type="checkbox" bind:checked={visible} />
-  visible
-</label>
-
-{#if visible}
-  <p transition:fade>Fades in and out</p>
-{/if} -->
